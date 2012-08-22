b7c03a41116bc22ab3f3796fe2d5aaa07ee321a6
Markus <mvbeck@gmail.com>
2012-08-22 15:48:27 -0300

    #3 WIP - Criado templates para o search form

--- tele/views.py
+++ tele/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from tele.models import Empresa, Telefone
+from tele.forms import SearchForm
 
 def home(request):
-    return TemplateResponse(request, 'tele/home.html', {'empresas': Empresa.objects.all()})
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            teste = form.cleaned_data['search_term']
+    return TemplateResponse(request, 'tele/home.html', {'empresas': Empresa.objects.all(), 'teste': teste})
